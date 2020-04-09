2e2f82003f77647b0452e782784feebc2f6e7b4a
Reagan Barrington <reagan@barringtonfamily.net>
2020-04-09 13:58:50 -0400

    api

--- api/api-router.js
+++ api/api-router.js
@@ -8,7 +8,7 @@ router.use(express.json());
 
 router.get("/", (req, res) => {
   const message = process.env.MESSAGE || "Hello from local host"
-  res.status(200).json({ message });
+  res.status(200).json({ api: "up" , message });
 });
 
 router.get("/shouts", (req, res, next) => {
